16363a7931848ee4c9a1b1d57cf201fafeb26b7f
shuaarageh <73986163+shuaarageh@users.noreply.github.com>
2021-10-24 11:46:35 +0300

    Add files via upload

---
+++ Shoaa1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE subject
+(
+course_id Number Not Null,
+course_Name Varchar2(50),
+faculity Varchar2(15)
+);
+insert INTO studenttt values (1,'anjale','www.gmail.c',20,5);
+insert INTO studenttt values (2,'anaa','www1.gmail.com',22,4);
+insert INTO studenttt values (3,'lana','www2.gmail.com',23,3);
+insert INTO studenttt values (4,'jameela','www3.gmail.com',20,5);
+
+insert INTO subject values (1,'math','amit');
+insert INTO subject values (2,'oracale','amit');
+insert INTO subject values (3,'unix','seema');
+insert INTO subject values (4,'network','seema');
+insert INTO subject values (10,'network0','seema');
